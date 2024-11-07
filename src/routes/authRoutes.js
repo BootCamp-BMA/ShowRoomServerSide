9d0d0485ccc3fa8660d048082cbe74623289eb4b
mbouch <mbouchaerb@gmail.com>
2024-11-07 21:13:30 +0100

    slove issues router.post not router.use

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express =require('express')
 const router= express.Router()
 const authController=require('../controllers/authController.js')
 
-router.use('/register',authController.register);
-router.use('/login',authController.login)
+router.post('/register',authController.register);
+router.post('/login',authController.login)
 
 
 
