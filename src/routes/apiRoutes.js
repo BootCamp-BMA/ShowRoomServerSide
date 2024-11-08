3131d4de5171b9e24538ff0ce88469500f3f6362
mbouch <mbouchaerb@gmail.com>
2024-11-08 19:21:13 +0100

    render to signup view

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -7,9 +7,6 @@ const authRootes=require('./authRoutes')
 router.use('/users',userRootes)
 router.use('/auth',authRootes)
 
-router.get('/hello',(req,res)=>{
-    res.json({message:'hello'})
-})
 
 
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,9 @@ const authController=require('../controllers/authController.js')
 router.post('/register',authController.register);
 router.post('/login',authController.login)
 
+router.get('/signup',(req,res,next)=>{
+    res.render('signup')
+})
 
 
 module.exports=router;
