c7825e1457549fa93cb3cbb022180eb206b633d5
mbouch <mbouchaerb@gmail.com>
2024-11-15 16:27:20 +0100

    .

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,5 @@ const imageMaxSize=parseInt(process.env.IMAGE_MAX_SIZE_MB)
 const model3dMaxSize=parseInt(process.env.MODEL3D_MAX_SIZE_MB)
 
 
+
 module.exports={secretKey,mongoUrl,imageMaxSize,model3dMaxSize}
