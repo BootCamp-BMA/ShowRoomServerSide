725f9b0a8e963aa39f958b2bb5a7e9f175f7f0d2
sohaib <souhabserrai1999@gmail.com>
2024-11-24 08:20:24 +0100

    "adding to the contact route"

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -5,12 +5,15 @@ const userRoutes=require('./userRoutes')
 const authRoutes=require('./authRoutes')
 const carRoutes=require('./carRoutes')
 const appointementRoutes=require('./appointementRoutes')
+const contactRoutes = require('./routes/contactRoutes');
 
 
 router.use('/users',userRoutes)
 router.use('/auth',authRoutes)
 router.use('/cars',carRoutes)
 router.use('/appointement',appointementRoutes)
+// Add this line to include the contact routes
+router.use('/contact', contactRoutes);
 
 
 
