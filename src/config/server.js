2b2234dcca424308e6261165bc9d63d8d9565f2e
sohaib <souhabserrai1999@gmail.com>
2024-11-24 08:23:51 +0100

    "same adding commantes"

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,17 @@
+// server.js
+// This file initializes the server, connects to the database,
+// and sets up the application to listen for incoming requests.
+
+// Loads environment variables from the .env file
 require('dotenv').config()
 
+
+// Imports the main application that defined in app.js
+// This file contains the Express application setup (e.g., routes, middlewares).
 const app = require('../app.js');
 
+
+// Imports the database connection functions from db.js
 const {connectMongo} = require('./db.js'); 
 
 
