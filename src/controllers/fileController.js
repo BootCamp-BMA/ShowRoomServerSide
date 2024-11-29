63cfeabe7df25babbd6181fae2d0a0ff99b7c62c
mbouch <mbouchaerb@gmail.com>
2024-11-29 14:46:53 +0100

    build strucute of controller file

---
+++ src/controllers/fileController.js
@@ -0,0 +1,32 @@
+module.exports.getFileById=async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.deletFileById=async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports.uploadModel=async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports.uploadImages=async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
--- src/routes/fileRoot.js
+++ src/routes/fileRoot.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const imageUpload=require('../middleware/uploadImages')
 const modelUpload=require('../middleware/uploadModel')
+const {deletFileById,getFileById,uploadModel, uploadImages} = require('../controllers/fileController')
 
-router.get('/getFileById/:id')
-router.delete('/deleteFileById/:id')
-router.post('/uploadModel',modelUpload.single('model3d'))
-router.post('/uploadImages',imageUpload.array('images', 5))
+router.get('/getFileById/:id',getFileById)
+router.delete('/deleteFileById/:id',deletFileById)
+router.post('/uploadModel',modelUpload.single('model3d'),uploadModel)
+router.post('/uploadImages',imageUpload.array('images', 5),uploadImages)
 
 module.exports=router
