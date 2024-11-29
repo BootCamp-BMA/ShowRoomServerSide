26a1c57134e3316c072e816b89724674493243ee
mbouch <mbouchaerb@gmail.com>
2024-11-29 14:41:11 +0100

    link multer to the root

--- src/routes/fileRoot.js
+++ src/routes/fileRoot.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const imageUpload=require('../middleware/uploadImages')
+const modelUpload=require('../middleware/uploadModel')
 
 router.get('/getFileById/:id')
 router.delete('/deleteFileById/:id')
-router.post('/uploadModel')
+router.post('/uploadModel',uploadModel.single('model3d'))
 router.post('/uploadImages',imageUpload.array('images', 5))
 
 module.exports=router
